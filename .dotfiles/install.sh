6f35b4c845bc8b24872e217d2da1b0da732e60f3
Yorgos <gtsitsik@gmail.com>
2026-01-15 01:09:04 +0200

    Make LSP server installation easier

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -5,5 +5,5 @@ gitc config --local status.showUntrackedFiles no
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -es -u ~/.vimrc -i NONE +"PlugInstall --sync"  +qa
-vim +"set ft=python" +"LspInstallServer ruff"
-vim +"set ft=python" +"LspInstallServer basedpyright-langserver"
+vim +"set ft=python" +"LspInstallServer! ruff"
+vim +"set ft=python" +"LspInstallServer! basedpyright-langserver"
