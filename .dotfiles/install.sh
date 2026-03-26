0a5f045ca417c3124cb41be2d5b36a8a25f6422e
Yorgos Tsitsikas <gtsitsik@gmail.com>
2026-03-26 22:00:26 +0200

    Fix installer

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git clone --bare https://github.com/gtsitsik/my-vim-settings.git "$HOME/.dotfiles"
-a='gitc(){ git --git-dir=\$HOME/.dotfiles --work-tree=\$HOME "$@" ; }'
+a='gitc(){ git --git-dir=$HOME/.dotfiles --work-tree=$HOME "$@" ; }'
 grep -Fq -- "$a" ~/.bashrc || echo "$a" >> ~/.bashrc
 eval "$a"
 gitc checkout
