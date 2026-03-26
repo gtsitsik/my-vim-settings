5831ad75a365b8bd2842e40200849ed9d6567d81
Yorgos Tsitsikas <gtsitsik@gmail.com>
2026-03-26 21:53:05 +0200

    Improve installer

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 git clone --bare https://github.com/gtsitsik/my-vim-settings.git "$HOME/.dotfiles"
-git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
 a='gitc(){ git --git-dir=\$HOME/.dotfiles --work-tree=\$HOME "$@" ; }'
-grep -Fq -- "$a" ~/.bashrc || { echo "$a" >> ~/.bashrc; source ~/.bashrc; }
+grep -Fq -- "$a" ~/.bashrc || echo "$a" >> ~/.bashrc
+eval "$a"
+gitc checkout
 gitc config --local status.showUntrackedFiles no
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
